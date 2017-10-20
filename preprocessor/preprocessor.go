501f383ebd91bd6730af1b0e06a6a3f3bd6d6f72
devplayg <devplayg@gmail.com>
2017-10-20 17:06:49 +0900

    setup preprocessor

--- preprocessor/preprocessor.go
+++ preprocessor/preprocessor.go
@@ -1,24 +1,94 @@
 package preprocessor
 
 import (
+	"crypto/md5"
 	"encoding/hex"
-	"errors"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-
-	"gopkg.in/h2non/filetype.v1"
-	//	"strconv"
-	//"path/filepath"
-	"crypto/md5"
-	"io/ioutil"
+	"regexp"
 	"time"
 
-	//	"github.com/devplayg/golibs/checksum"
-	//	"github.com/devplayg/golibs/strings"
+	//	"github.com/davecgh/go-spew/spew"
 	"github.com/fsnotify/fsnotify"
+	"github.com/mholt/archiver"
 	"github.com/satori/go.uuid"
 	log "github.com/sirupsen/logrus"
+	"gopkg.in/h2non/filetype.v1"
 )
+
+type File struct {
+	UUID      string
+	P_UUID    string
+	G_UUID    string
+	Name      string
+	Path      string
+	MD5       string
+	Size      uint64
+	Priority  uint8
+	Type      string
+	Extension string
+	Mime      string
+	State     uint16
+	Depth     int
+	Children  []File
+}
+
+func newFile(path, p_uuid, g_uuid string, depth int) (*File, error) {
+	file := File{
+		Path:     path,
+		UUID:     uuid.NewV4().String(),
+		P_UUID:   p_uuid,
+		G_UUID:   g_uuid,
+		Name:     filepath.Base(path),
+		Depth:    depth,
+		Children: make([]File, 0, 1),
+	}
+
+	// Sleep
+	time.Sleep(100 * time.Millisecond)
+
+	// Read file
+	data, err := ioutil.ReadFile(file.Path)
+	if err != nil {
+		log.Error(err)
+		count := 1
+		for err != nil && count <= 15 {
+			time.Sleep(1 * time.Second)
+			data, err = ioutil.ReadFile(file.Path)
+			if err != nil {
+				log.Error(err)
+			}
+			count++
+		}
+
+		if err != nil {
+			//			this.errChan <- errors.New("Failed to read file: " + file.Path)
+			return &file, err
+		}
+	}
+
+	// Get MD5 checksum
+	b := md5.Sum(data)
+	file.MD5 = hex.EncodeToString(b[:16])
+	file.Size = uint64(len(data))
+
+	// Filetype
+	filetype, _ := filetype.Match(data)
+	file.Type = filetype.Extension
+	file.Mime = filetype.MIME.Value
+	//	log.Infof("File type: %s. MIME: %s\n", filetype.Extension, filetype.MIME.Value)
+	//	spew.Dump(file)
+	// Move file
+	//	//	err = os.Rename(name, filepath.Join(this.homedir, "/storage", md5[0:2] ,md5+".bin"))
+	//	err = os.Rename(file.Path, filepath.Join(this.homedir, "/storage", file.MD5+".bin"))
+	//	if err != nil {
+	//		this.errChan <- err
+	//		return &file, err
+	//	}
+
+	return &file, nil
+}
 
 type Preprocessor struct {
 	homedir string
@@ -79,15 +149,32 @@ func (this *Preprocessor) Start(errChan chan<- error) error {
 			select {
 			case event := <-watcher.Events:
 				if event.Op == fsnotify.Create {
-
 					this.c <- true
-					this.enqueue(event.Name, 0)
+					file, err := newFile(event.Name, "", "", 0)
+					if err != nil {
+						errChan <- err
+						<-this.c
+						//						continue
+					} else {
+						this.enqueue(file)
+					}
+
+					//					// Issue UUID
+					//					uuid := uuid.NewV4().String()
+					//					file := File{
+					//						Path:   event.Name,
+					//						Depth:  0,
+					//						UUID:   uuid,
+					//						G_UUID: uuid,
+					//					}
+					//					uuid :=
+					//					file := newFile(event.Name, p_uuid, g_uuid)
+
 				}
 				//				if event.Op&fsnotify.Write == fsnotify.Write {
 				//					log.Println("modified file:", event.Name)
 				//				}
 			case err := <-watcher.Errors:
-				//				log.Println("event:###")
 				this.errChan <- err
 			}
 		}
@@ -108,60 +195,62 @@ func (this *Preprocessor) Stop() error {
 	return nil
 }
 
-func (this *Preprocessor) enqueue(name string, depth int) error { // uuid, p_uuid, g_uuid
+func (this *Preprocessor) enqueue(file *File) error { // uuid, p_uuid, g_uuid, -- priority, filesize, type,
+	log.Infof("Mime: %s, Type: %s\n", file.Mime, file.Type)
 	defer func() {
 		<-this.c
+
 	}()
 
-	// Sleep
-	time.Sleep(100 * time.Millisecond)
+	var err error
+	// Extract
+	if match, _ := regexp.MatchString("^(zip|7z)$", file.Type); match {
+		tempDir := this.tempMkdir()
 
-	// Read file
-	data, err := ioutil.ReadFile(name)
-	if err != nil {
-		log.Info("###x")
-		this.errChan <- err
-		count := 1
-		for err != nil && count <= 15 {
-			time.Sleep(1 * time.Second)
-			data, err = ioutil.ReadFile(name)
-			if err != nil {
-				this.errChan <- err
-			}
-			count++
+		switch file.Type {
+		case "zip":
+			err = archiver.Zip.Open(file.Path, tempDir)
+		case "7z":
+			err = archiver.TarGz.Open(file.Path, tempDir)
+			//			log.Info("#")
+			//			fmt.Println("Linux.")
+		default:
+			log.Info("#")
+			// freebsd, openbsd,
+			// plan9, windows...
+			//			fmt.Printf("%s.", os)
 		}
 
 		if err != nil {
-			this.errChan <- errors.New("Failed to read file: " + name)
-			return err
+			this.errChan <- err
 		}
+		//		if file.Type == "zip" {
+		//			err := archiver.Zip.Open(file.Path, tempDir)
+		//		} else
 	}
 
-	// Get MD5 checksum
-	b := md5.Sum(data)
-	md5 := hex.EncodeToString(b[:16])
-	log.Debugf("Checksum=%s", md5)
+	//	log.Info(tempDir)
 
-	// Filetype
-	filetype, err2 := filetype.Match(data)
-	if err2 != nil {
-		this.errChan <- err
-		return nil
-	}
-	log.Infof("File type: %s. MIME: %s\n", filetype.Extension, filetype.MIME.Value)
+	//	// Extract
+	//	tempDir := "tempDir"
+	//	ReadDir(tempDir)
+	//	for ReadFile(tempDir) {
+	//		f := newFile(path, file.UUID, file.G_UUID, file.Depth+1)
+
+	//		this.enqueue(f)
+	//	}
 
 	// Move file
 	//	err = os.Rename(name, filepath.Join(this.homedir, "/storage", md5[0:2] ,md5+".bin"))
-	err = os.Rename(name, filepath.Join(this.homedir, "/storage", md5+".bin"))
-
+	err = os.Rename(file.Path, filepath.Join(this.homedir, "/storage", file.MD5+".bin"))
 	if err != nil {
 		this.errChan <- err
 		return err
 	}
+	log.Infof("Rename complete: %s ", file.Path)
 
 	// Issue UUID
-	uuid := uuid.NewV4()
-	log.Debug(uuid)
+	//	file.UUID = uuid.NewV4().String()
 
 	// Get type
 
@@ -176,7 +265,17 @@ func (this *Preprocessor) enqueue(name string, depth int) error { // uuid, p_uui
 	// Write log
 	//	time.Sleep(2 * time.Second)
 	//	log.Debug("Enqueued: " + filepath)
+	//	spew.Dump(file)
+
 	return nil
+}
+
+func (this *Preprocessor) tempMkdir() string {
+	dir, err := ioutil.TempDir("/home/fas/temp", "fas")
+	if err != nil {
+		this.errChan <- err
+	}
+	return dir
 }
 
 func (this *Preprocessor) init() {
